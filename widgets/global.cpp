ffe73f16228d33836a9940fcfdaddac2bf54b3bb
wangwenx190 <2546789017@qq.com>
2018-11-09 15:39:33 +0800

    Merged pull request ##1155 from sundayglee: Update Subtitle.cpp

--- widgets/global.cpp
+++ widgets/global.cpp
@@ -20,12 +20,7 @@
 ******************************************************************************/
 
 #include "QtAVWidgets/global.h"
-#include <QApplication>
-#include <QBoxLayout>
 #include <QMessageBox>
-#include <QPushButton>
-#include <QTabWidget>
-#include <QTextBrowser>
 
 #include "QtAVWidgets/WidgetRenderer.h"
 #include "QtAVWidgets/GraphicsItemRenderer.h"
@@ -129,32 +124,7 @@ namespace {
 }
 
 void about() {
-    //we should use new because a qobject will delete it's children
-    QTextBrowser *viewQtAV = new QTextBrowser;
-    QTextBrowser *viewFFmpeg = new QTextBrowser;
-    viewQtAV->setOpenExternalLinks(true);
-    viewFFmpeg->setOpenExternalLinks(true);
-    viewQtAV->setHtml(aboutQtAV_HTML());
-    viewFFmpeg->setHtml(aboutFFmpeg_HTML());
-    QTabWidget *tab = new QTabWidget;
-    tab->addTab(viewQtAV, QStringLiteral("QtAV"));
-    tab->addTab(viewFFmpeg, QStringLiteral("FFmpeg"));
-    QPushButton *qbtn = new QPushButton(QObject::tr("About Qt"));
-    QPushButton *btn = new QPushButton(QObject::tr("Ok"));
-    QHBoxLayout *btnLayout = new QHBoxLayout;
-    btnLayout->addWidget(btn);
-    btnLayout->addStretch();
-    btnLayout->addWidget(qbtn);
-    btn->setFocus();
-    QDialog dialog;
-    dialog.setWindowTitle(QObject::tr("About") + QStringLiteral("  QtAV"));
-    QVBoxLayout *layout = new QVBoxLayout;
-    dialog.setLayout(layout);
-    layout->addWidget(tab);
-    layout->addLayout(btnLayout);
-    QObject::connect(qbtn, SIGNAL(clicked()), qApp, SLOT(aboutQt()));
-    QObject::connect(btn, SIGNAL(clicked()), &dialog, SLOT(accept()));
-    dialog.exec();
+    aboutQtAV();
 }
 
 void aboutFFmpeg()
